bb627ce2f1956c46e99f070aed739647e0c1fac6
TomB <tomasz.m.bak@gmail.com>
2026-02-03 11:35:25 -0800

    Initial Commit

---
+++ LIFXBTMacApp/LIFXBTMacAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LIFXBTMacAppApp.swift
+//  LIFXBTMacApp
+//
+//  Created by Tomasz Bak on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct LIFXBTMacAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
